8f70a8dc9526a861432caaff0aa6bbc9bfba5ffd
luoaho <1241056256@qq.com>
2021-01-24 15:22:35 +0800

    '移动端延迟图片懒加载'

--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import App from './App.vue'
 import router from "./router";
 import store from "./store";
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 import toast from 'components/common/toast/index'
 Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 //安装toast
 Vue.use(toast)
+
+//解决移动端300毫秒的延迟
+FastClick.attach(document.body)
+
+//使用图片懒加载的插件
+Vue.use(VueLazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
